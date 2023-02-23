1e3006801733629d33391855e7cca6a99959b3a0
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-23 10:52:08 +0100

    modules, import and export

--- modules/mainscript.js
+++ modules/mainscript.js
@@ -1,13 +1,23 @@
 "use strict"
 
-window.addEventListener("DOMContentLoaded", start);
+// import {capitalize, makeSpaces} from "./stringscript.js"; 
+import * as stringTools from "./stringscript.js";
+
+// we don't need this because modules promise that the DOM is fully loaded before run the script. In addition, you can move script links up in the HTML (take a look at the HTML)
+// window.addEventListener("DOMContentLoaded", start);ù
+// MODULES ONLY RUN ON LIVE SERVER OR SERVERS
+
+start();
 
 function start() {
   console.log("Start!");
-
   sayHello("peter");
 }
 
 function sayHello( name ) {
-  console.log(`Hello ${capitalize(name)}`);
+  // const greeting = makeSpaces(`Hello ${capitalize(name)}!`);
+  const greeting = stringTools.makeSpaces(`Hello ${stringTools.capitalize(name)}!`);
+  document.querySelector("#output").textContent = greeting;
+  console.log("greeting:", greeting);
 }
+
